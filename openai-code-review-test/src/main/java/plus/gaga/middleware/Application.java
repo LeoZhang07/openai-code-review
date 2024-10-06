a56952da31f000b3f2114d83275ea9d7f2656c07
leo <1027608831@qq.com>
2024-10-06 16:50:15 +0800

    feat：工程适配 v1.5 let's run!

--- openai-code-review-test/src/main/java/plus/gaga/middleware/Application.java
+++ openai-code-review-test/src/main/java/plus/gaga/middleware/Application.java
@@ -5,5 +5,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("代码评审测试");
         System.out.println("for action testing");
+        System.out.println("let's run!");
     }
 }
