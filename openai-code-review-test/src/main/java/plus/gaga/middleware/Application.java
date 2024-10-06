b071e17121a08f8aca4d83c4fbe2f0eafa30bcf2
leo <1027608831@qq.com>
2024-10-06 09:58:25 +0800

    feat：工程适配 v1.2 change branch for action testing

--- openai-code-review-test/src/main/java/plus/gaga/middleware/Application.java
+++ openai-code-review-test/src/main/java/plus/gaga/middleware/Application.java
@@ -4,5 +4,6 @@ package plus.gaga.middleware;
 public class Application {
     public static void main(String[] args) {
         System.out.println("代码评审测试");
+        System.out.println("for action testing");
     }
 }
