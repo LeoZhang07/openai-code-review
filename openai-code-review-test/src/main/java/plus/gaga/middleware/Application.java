186f50a7096c162cf67e3d264c14aab539f2e8f3
leo <1027608831@qq.com>
2024-10-06 17:01:56 +0800

    feat：工程适配 v1.7 updated the sdk and run again!

--- openai-code-review-test/src/main/java/plus/gaga/middleware/Application.java
+++ openai-code-review-test/src/main/java/plus/gaga/middleware/Application.java
@@ -5,6 +5,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("代码评审测试");
         System.out.println("for action testing");
-        System.out.println("let's run!");
+        System.out.println("let's run again!");
     }
 }
