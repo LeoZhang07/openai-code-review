c8fe8ceb370fde53f5761bb5b5797f6b5b8832a1
leo <1027608831@qq.com>
2024-10-06 16:57:43 +0800

    feat：工程适配 v1.6 debug find the empty key

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -12,25 +12,6 @@ public class OpenAiCodeReview {
 
     private static final Logger logger = LoggerFactory.getLogger(OpenAiCodeReview.class);
 
-    // wx配置配置
-    private String weixin_appid = "wxd407f6742f84a93f";
-    private String weixin_secret = "c977f75bae661a41914ff32e1c54c78d";
-    private String weixin_touser = "oLuWP6L5qkSGQm1JqjccpeCh7-jU";
-    private String weixin_template_id = "ebeH7bkbdjVrfBFAHqkksRre75XBJjsE-7VIrhY35MQ";
-
-    // ChatGLM 配置
-    private String chatglm_apiHost = "https://open.bigmodel.cn/api/paas/v4/chat/completions";
-    private String chatglm_apiKeySecret = "e084e7abb3c3db14f8af16dc15b96910.oI9CwFiLsBRuX0BE";
-
-    // 代码评审结果存在 github上
-    private String code_review_log_uri;
-    private String github_token;
-
-    // 工程配置 - 自动获取
-    private String github_project;
-    private String github_branch;
-    private String github_author;
-
     public static void main(String[] args) throws Exception {
         GitCommand gitCommand = new GitCommand(
                 getEnv("CODE_REVIEW_LOG_URI"),
@@ -52,7 +33,6 @@ public class OpenAiCodeReview {
         );
 
 
-
         IOpenAI openAI = new ChatGLM(getEnv("CHATGLM_APIHOST"), getEnv("CHATGLM_APIKEYSECRET"));
 
         OpenAiCodeReviewService openAiCodeReviewService = new OpenAiCodeReviewService(gitCommand, openAI, weiXin);
@@ -70,7 +50,7 @@ public class OpenAiCodeReview {
     private static String getEnv(String key) {
         String value = System.getenv(key);
         if (null == value || value.isEmpty()) {
-            throw new RuntimeException("value is null");
+            throw new RuntimeException("the key: " + key + " mapping value is null");
         }
         return value;
     }
