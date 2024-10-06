5530be001d416a2d9cf5f2766b1cf886a6f67a27
leo <1027608831@qq.com>
2024-10-06 17:11:06 +0800

    feat：工程适配 v1.8 fixed the missing key,update the sdk ,and run again!

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -15,7 +15,7 @@ public class OpenAiCodeReview {
     public static void main(String[] args) throws Exception {
         GitCommand gitCommand = new GitCommand(
                 getEnv("CODE_REVIEW_LOG_URI"),
-                getEnv("GITHUB_TOKEN"),
+                getEnv("CODE_REVIEW_LOG_PUSH_GITHUB_TOKEN"),
                 getEnv("COMMIT_PROJECT"),
                 getEnv("COMMIT_BRANCH"),
                 getEnv("COMMIT_AUTHOR"),
